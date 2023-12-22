fc9597f0a56262e2b9c055377698d9a4e39f7b82
Teemu Tuominiemi <teemu.v.tuominiemi@gmail.com>
2023-12-22 23:24:35 +0200

    dev

--- melee nerf/media/lua/client/lanttuMeleeNerfClient.lua
+++ melee nerf/media/lua/client/lanttuMeleeNerfClient.lua
@@ -8,254 +8,191 @@ LanttuMeleeNerf.MoveDirection = {
 }
 
 LanttuMeleeNerf.walkDirection = function (character)
-	if not character:isPlayerMoving() then return LanttuMeleeNerf.MoveDirection.STADING end
+	if not character:isPlayerMoving() then return LanttuMeleeNerf.MoveDirection.STANDING end
 
-	local direction = character:getPlayerMoveDir():getDirection();
-	local look_angle = character:getDirectionAngle();
+	local move_direction = character:getPlayerMoveDir():getDirection()
+	local look_angle = character:getDirectionAngle()
 	
-	if direction > 0.78 and direction < 0.79 then
-		--print("WALKING [N]");
+	if move_direction > 0.78 and move_direction < 0.79 then
+		--print("WALKING [N]")
 		if look_angle <= -45 and look_angle >= -135 then
-			print("WALKING [N] FORWARD");
+			--print("WALKING [N] FORWARD")
 			return LanttuMeleeNerf.MoveDirection.FORWARD
 		elseif look_angle >= 45 and look_angle <= 135 then
-			print("WALKING [N] BACKWARD");
+			--print("WALKING [N] BACKWARD")
 			return LanttuMeleeNerf.MoveDirection.BACKWARD
 		end 
-	elseif direction == 0 then
-		--print("WALKING [NE]");
+	elseif move_direction == 0 then
+		--print("WALKING [NE]")
 		if look_angle >= -90 and look_angle <= -0 then
-			print("WALKING [NE] FORWARD");
+			--print("WALKING [NE] FORWARD")
 			return LanttuMeleeNerf.MoveDirection.FORWARD
 		elseif look_angle <= 180 and look_angle >= 90 then
-			print("WALKING [NE] BACKWARD");
+			--print("WALKING [NE] BACKWARD")
 			return LanttuMeleeNerf.MoveDirection.BACKWARD
 		end 
-	elseif direction < -0.78 and direction > -0.79 then
-		--print("WALKING [E]");
+	elseif move_direction < -0.78 and move_direction > -0.79 then
+		--print("WALKING [E]")
 		if look_angle >= -45 and look_angle <= 45 then
-			print("WALKING [E] FORWARD");
+			--print("WALKING [E] FORWARD")
 			return LanttuMeleeNerf.MoveDirection.FORWARD
 		elseif (look_angle >= 135 and look_angle <= 180) or (look_angle <= -135 and look_angle >= -180) then
-			print("WALKING [E] BACKWARD");
+			--print("WALKING [E] BACKWARD")
 			return LanttuMeleeNerf.MoveDirection.BACKWARD
 		end 
-	elseif direction < -1.57 and direction > -1.58 then
-		--print("WALKING [SE]");
+	elseif move_direction < -1.57 and move_direction > -1.58 then
+		--print("WALKING [SE]")
 		if look_angle >= 0 and look_angle <= 90 then
-			print("WALKING [SE] FORWARD");
+			--print("WALKING [SE] FORWARD")
 			return LanttuMeleeNerf.MoveDirection.FORWARD
 		elseif look_angle <= -90 and look_angle >= -180 then
-			print("WALKING [SE] BACKWARD");
+			--print("WALKING [SE] BACKWARD")
 			return LanttuMeleeNerf.MoveDirection.BACKWARD
 		end 
-	elseif direction < -2.35 and direction > -2.36 then
-		--print("WALKING [S]");
+	elseif move_direction < -2.35 and move_direction > -2.36 then
+		--print("WALKING [S]")
 		if look_angle >= 45 and look_angle <= 135 then
-			print("WALKING [S] FORWARD");
+			--print("WALKING [S] FORWARD")
 			return LanttuMeleeNerf.MoveDirection.FORWARD
 		elseif look_angle <= -45 and look_angle >= -135 then
-			print("WALKING [S] BACKWARD");
+			--print("WALKING [S] BACKWARD")
 			return LanttuMeleeNerf.MoveDirection.BACKWARD
 		end 
-	elseif direction < -3.14 and direction > -3.15 then
-		--print("WALKING [SW]");
+	elseif move_direction < -3.14 and move_direction > -3.15 then
+		--print("WALKING [SW]")
 		if look_angle <= 180 and look_angle >= 90 then
-			print("WALKING [SW] FORWARD");
+			--print("WALKING [SW] FORWARD")
 			return LanttuMeleeNerf.MoveDirection.FORWARD
 		elseif look_angle >= -90 and look_angle <= -0 then
-			print("WALKING [SW] BACKWARD");
+			--print("WALKING [SW] BACKWARD")
 			return LanttuMeleeNerf.MoveDirection.BACKWARD
 		end 
-	elseif direction > 2.35 and direction < 2.36 then
-		--print("WALKING [W]");
+	elseif move_direction > 2.35 and move_direction < 2.36 then
+		--print("WALKING [W]")
 		if (look_angle >= 135 and look_angle <= 180) or (look_angle <= -135 and look_angle >= -180) then
-			print("WALKING [W] FORWARD");
+			--print("WALKING [W] FORWARD")
 			return LanttuMeleeNerf.MoveDirection.FORWARD
 		elseif look_angle >= -45 and look_angle <= 45 then
-			print("WALKING [W] BACKWARD");
+			--print("WALKING [W] BACKWARD")
 			return LanttuMeleeNerf.MoveDirection.BACKWARD
 		end 
-	elseif direction > 1.57 and direction < 1.58 then
-		--print("WALKING [NW]");
+	elseif move_direction > 1.57 and move_direction < 1.58 then
+		--print("WALKING [NW]")
 		if look_angle <= -90 and look_angle >= -180 then
-			print("WALKING [NW] FORWARD");
+			--print("WALKING [NW] FORWARD")
 			return LanttuMeleeNerf.MoveDirection.FORWARD
 		elseif look_angle >= 0 and look_angle <= 90 then
-			print("WALKING [NW] BACKWARD");
+			--print("WALKING [NW] BACKWARD")
 			return LanttuMeleeNerf.MoveDirection.BACKWARD
 		end 
 	end
 	return LanttuMeleeNerf.MoveDirection.SIDEWAYS
 end
 
-LanttuMeleeNerf.initializeCharacterAttack = function (character)
-	--print(string.format('Orig CRIT [%f]', hand_weapon:getCriticalChance()));
-	--print(string.format('Orig KnockDown [%s]', tostring(hand_weapon:isAlwaysKnockdown())));
-	--print(string.format('Orig minDamage [%f]', hand_weapon:getMinDamage()));
-	--print(string.format('Orig maxDamage [%f]', hand_weapon:getMaxDamage()));
-	local walk_direction = LanttuMeleeNerf.walkDirection(character);
-	
-	if walk_direction ~= LanttuMeleeNerf.MoveDirection.FORWARD then
-		character:setCriticalHit(false);
-	end
-
-	character:getModData().meleenerf = {}
-	character:getModData().meleenerf.walk_direction = walk_direction;
-end
-
-LanttuMeleeNerf.releaseCharacterAttack = function (character)
-	--print(string.format('Orig CRIT [%f]', hand_weapon:getCriticalChance()));
-	--print(string.format('Orig KnockDown [%s]', tostring(hand_weapon:isAlwaysKnockdown())));
-	--print(string.format('Orig minDamage [%f]', hand_weapon:getMinDamage()));
-	--print(string.format('Orig maxDamage [%f]', hand_weapon:getMaxDamage()));
-	character:getModData().meleenerf = {}
+LanttuMeleeNerf.isCriticalHit = function (hand_weapon, multiplier)
+	local random = ZombRand(0,101)
+	local hit_chance = hand_weapon:getCriticalChance() * multiplier
+	return random <= hit_chance
 end
 
 LanttuMeleeNerf.initializeWeaponStats = function (hand_weapon)
-	--print(string.format('Orig CRIT [%f]', hand_weapon:getCriticalChance()));
-	--print(string.format('Orig KnockDown [%s]', tostring(hand_weapon:isAlwaysKnockdown())));
-	--print(string.format('Orig minDamage [%f]', hand_weapon:getMinDamage()));
-	--print(string.format('Orig maxDamage [%f]', hand_weapon:getMaxDamage()));
-	
 	hand_weapon:getModData().meleenerf = {}
-	hand_weapon:getModData().meleenerf.criticalChance = hand_weapon:getCriticalChance();
-	hand_weapon:getModData().meleenerf.isAlwaysKnockdown = hand_weapon:isAlwaysKnockdown();
-	hand_weapon:getModData().meleenerf.minDamage = hand_weapon:getMinDamage();
-	hand_weapon:getModData().meleenerf.maxDamage = hand_weapon:getMaxDamage();
-	hand_weapon:getModData().meleenerf.knockbackOnNoDeath = hand_weapon:isKnockBackOnNoDeath();
+	hand_weapon:getModData().meleenerf.minDamage = hand_weapon:getMinDamage()
+	hand_weapon:getModData().meleenerf.maxDamage = hand_weapon:getMaxDamage()
 end
 
 LanttuMeleeNerf.restoreWeaponStats = function (hand_weapon)
-	--print(string.format('Restore CRIT [%f] -> [%f]', hand_weapon:getCriticalChance(), hand_weapon:getModData().meleenerf.criticalChance));
-	--print(string.format('Restore KnockDown [%s] -> [%s]', tostring(hand_weapon:isAlwaysKnockdown()), tostring(hand_weapon:getModData().meleenerf.isAlwaysKnockdown)));
-	--print(string.format('Restore minDamage [%f] -> [%f]', hand_weapon:getMinDamage(), hand_weapon:getModData().meleenerf.minDamage));
-	--print(string.format('Restore maxDamage [%f] -> [%f]', hand_weapon:getMaxDamage(), hand_weapon:getModData().meleenerf.maxDamage));
-	hand_weapon:setCriticalChance(hand_weapon:getModData().meleenerf.criticalChance);
-	hand_weapon:setAlwaysKnockdown(hand_weapon:getModData().meleenerf.isAlwaysKnockdown);
-	hand_weapon:setMinDamage(hand_weapon:getModData().meleenerf.minDamage);
-	hand_weapon:setMaxDamage(hand_weapon:getModData().meleenerf.maxDamage);
-	hand_weapon:setKnockBackOnNoDeath(hand_weapon:getModData().meleenerf.knockbackOnNoDeath);
+	hand_weapon:setMinDamage(hand_weapon:getModData().meleenerf.minDamage)
+	hand_weapon:setMaxDamage(hand_weapon:getModData().meleenerf.maxDamage)
 end
 
 LanttuMeleeNerf.doNerf = function (character, hand_weapon)
-	print("NERF NERF");
-	LanttuMeleeNerf.initializeWeaponStats(hand_weapon);
-	hand_weapon:setCriticalChance(0);
-	hand_weapon:setAlwaysKnockdown(false);
-	hand_weapon:setMinDamage(hand_weapon:getModData().meleenerf.minDamage * 0);
-	hand_weapon:setMaxDamage(hand_weapon:getModData().meleenerf.maxDamage * 0);
-	hand_weapon:setKnockBackOnNoDeath(false);
+	print("NERF NERF")
+	LanttuMeleeNerf.initializeWeaponStats(hand_weapon)
+	hand_weapon:setMinDamage(hand_weapon:getModData().meleenerf.minDamage * 0.4)
+	hand_weapon:setMaxDamage(hand_weapon:getModData().meleenerf.maxDamage * 0.4)
+
+	if LanttuMeleeNerf.isCriticalHit(hand_weapon, 0.1) then
+		character:setCriticalHit(true)
+	end
 end
 
 LanttuMeleeNerf.doBoost = function (character, hand_weapon)
-	print("BOOST BOOST");
-	LanttuMeleeNerf.doNerf(character, hand_weapon);
-	--LanttuMeleeNerf.initializeWeaponStats(hand_weapon);
-	--hand_weapon:setCriticalChance(100);
-	--hand_weapon:setAlwaysKnockdown(true);
-	--hand_weapon:setMinDamage(hand_weapon:getModData().meleenerf.minDamage * 3);
-	--hand_weapon:setMaxDamage(hand_weapon:getModData().meleenerf.maxDamage * 3);
+	print("BOOST BOOST")
+	LanttuMeleeNerf.initializeWeaponStats(hand_weapon)
+	hand_weapon:setMinDamage(hand_weapon:getModData().meleenerf.minDamage * 1.2)
+	hand_weapon:setMaxDamage(hand_weapon:getModData().meleenerf.maxDamage * 1.2)
+
+	if LanttuMeleeNerf.isCriticalHit(hand_weapon, 1.4) then
+		character:setCriticalHit(true)
+	end
 end
 
 LanttuMeleeNerf.doBasic = function (character, hand_weapon)
-	print("DO BASIC");
-	--LanttuMeleeNerf.initializeWeaponStats(hand_weapon);
-	LanttuMeleeNerf.doNerf(character, hand_weapon);
+	print("DO BASIC")
+	LanttuMeleeNerf.initializeWeaponStats(hand_weapon)
 end
 
 LanttuMeleeNerf.OnWeaponSwing = function (character, hand_weapon)
-	if not character or character:isDead() then return end;
-	if not hand_weapon then return end;
-	if hand_weapon:isRanged() then return end;
+	if not character or character:isDead() then return end
+	if not hand_weapon then return end
+	if hand_weapon:isRanged() then return end
 
-	LanttuMeleeNerf.initializeCharacterAttack(character);
-end
-
-LanttuMeleeNerf.OnWeaponSwingHitPoint = function (character, hand_weapon)
-	if not character or character:isDead() then return end;
-	if not hand_weapon then return end;
-	if hand_weapon:isRanged() then return end;
-
-	local move_direction = character:getModData().meleenerf.walk_direction;
-	if move_direction == LanttuMeleeNerf.MoveDirection.FORWARD then
-		LanttuMeleeNerf.doBoost(character, hand_weapon);
-	elseif move_direction == LanttuMeleeNerf.MoveDirection.BACKWARD then
-		LanttuMeleeNerf.doNerf(character, hand_weapon);
+	local walk_direction = LanttuMeleeNerf.walkDirection(character)
+	if walk_direction == LanttuMeleeNerf.MoveDirection.FORWARD then
+		LanttuMeleeNerf.doBoost(character, hand_weapon)
+	elseif walk_direction == LanttuMeleeNerf.MoveDirection.BACKWARD then
+		LanttuMeleeNerf.doNerf(character, hand_weapon)
 	else
 		LanttuMeleeNerf.doBasic(character, hand_weapon)
 	end
 end
 
-LanttuMeleeNerf.OnPlayerAttackFinished = function (character, hand_weapon)
-	if not character or character:isDead() then return end;
-	if not hand_weapon then return end;
-	if hand_weapon:isRanged() then return end;
-	LanttuMeleeNerf.releaseCharacterAttack(character);
-	LanttuMeleeNerf.restoreWeaponStats(hand_weapon);
+LanttuMeleeNerf.OnWeaponSwingHitPoint = function (character, hand_weapon)
 end
 
-LanttuMeleeNerf.OnWeaponHitCharacter = function (actor, target, weapon, damage)
-	if not actor then return end;
-	if not target then return end;
-	if not weapon then return end;
-	--print("OnWeaponHitCharacter - actor[" ..actor.. "], target[" ..target.. "], weapon[" ..weapon.. "], damage[" ..damage.. "]")
-	print(string.format('HIT WITH DAMAGE [%s] HEALTH: [%f] isDEAD: %s', tostring(damage or "nil"), target:getHealth(), tostring(target:isDead())));
+LanttuMeleeNerf.OnPlayerAttackFinished = function (character, hand_weapon)
+	if not hand_weapon then return end
+	if hand_weapon:isRanged() then return end
+	LanttuMeleeNerf.restoreWeaponStats(hand_weapon)
+end
+
+LanttuMeleeNerf.OnWeaponHitCharacter = function (character, target, hand_weapon, damage)
+	if not character then return end
+	if not target then return end
+	if not hand_weapon then return end
+	if hand_weapon:isRanged() then return end
+	print(string.format('HIT WITH DAMAGE [%s] HEALTH: [%f] isDEAD: %s', tostring(damage or "nil"), target:getHealth(), tostring(target:isDead())))
 end
 
 LanttuMeleeNerf.OnHitZombie = function (zombie, character, body_part_type, hand_weapon)
-	if not zombie then return end;
-	if not character then return end;
-	if not hand_weapon then return end;
-	print("OnHitZombie");
-	print(string.format('Zombie HEALTH: %f', zombie:getHealth()));
-	--print("OnHitZombie - zombie[" ..zombie.. "], character[" ..character.. "], bodyPartType[" ..body_part_type.. "], handWeapon[" ..hand_weapon.."]")
+	if not zombie then return end
+	if not character then return end
+	if not hand_weapon then return end
+	if hand_weapon:isRanged() then return end
+	--print(string.format('Zombie HEALTH: %f', zombie:getHealth()))
 end
 
 LanttuMeleeNerf.OnPlayerMove = function (character)
-	if character:isAiming() then
-		--print(string.format('knockbackAttachMod: %f', character:knockbackAttackMod()));
-		--LanttuMeleeNerf.walkDirection(character);
-		--local move_dir_x = player:getPlayerMoveDir():getX();
-		--local move_dir_y = player:getPlayerMoveDir():getY();
-		--local direction = character:getPlayerMoveDir():getDirection();
-		--local direction_angle = character:getDirectionAngle();
-		--local last_angle = player:getLastAngle():getDirection();
-		--local aim_dir_x = player:getAimVector():getX();
-		--local aim_dir_y = player:getAimVector():getY();
-		--print(string.format('DIRECTION ANGLE [%f] DIRECTION [%f]', direction_angle, direction));
-	end
 end
 
 LanttuMeleeNerf.OnPlayerUpdate = function (player)
-	--if player:isAiming() then
-		--print("MOVING AND AIMING" ..player:getDisplayName().." DIRECTION ANGLE: " ..player:getDirectionAngle());
-		--local direction_angle = player:getDirectionAngle();
-		--local last_angle = player:getLastAngle():getDirection();
-		--local aim_dir_x = player:getAimVector():getX();
-		--local aim_dir_y = player:getAimVector():getY();
-		
-		--print(string.format('ON PLAYUPDATE DIRECTION ANGLE [%f] last_angle [%f]', direction_angle, last_angle));
-	--end
 end
 
 LanttuMeleeNerf.AddXP = function (character, perk, level)
-	print(string.format("XP GAINED [%f]", level));
+	--print(string.format("XP GAINED [%f]", level))
 end
 
 LanttuMeleeNerf.InitializeEvents = function (player)
-
+	Events.OnWeaponSwing.Add(LanttuMeleeNerf.OnWeaponSwing)
+	Events.OnWeaponSwingHitPoint.Add(LanttuMeleeNerf.OnWeaponSwingHitPoint)
+	Events.OnWeaponHitCharacter.Add(LanttuMeleeNerf.OnWeaponHitCharacter)
+	Events.OnHitZombie.Add(LanttuMeleeNerf.OnHitZombie)
+	Events.OnPlayerAttackFinished.Add(LanttuMeleeNerf.OnPlayerAttackFinished)
+	Events.OnPlayerMove.Add(LanttuMeleeNerf.OnPlayerMove)
+	Events.OnPlayerUpdate.Add(LanttuMeleeNerf.OnPlayerUpdate)
+	Events.AddXP.Add(LanttuMeleeNerf.AddXP)
 end
 
-print("Lanttuchef's Melee weapon initialize events")
-Events.OnWeaponSwing.Add(LanttuMeleeNerf.OnWeaponSwing);
-Events.OnWeaponSwingHitPoint.Add(LanttuMeleeNerf.OnWeaponSwingHitPoint);
-Events.OnWeaponHitCharacter.Add(LanttuMeleeNerf.OnWeaponHitCharacter);
-Events.OnHitZombie.Add(LanttuMeleeNerf.OnHitZombie);
-Events.OnPlayerAttackFinished.Add(LanttuMeleeNerf.OnPlayerAttackFinished);
-Events.OnPlayerMove.Add(LanttuMeleeNerf.OnPlayerMove);
-Events.OnPlayerUpdate.Add(LanttuMeleeNerf.OnPlayerUpdate);
-Events.AddXP.Add(LanttuMeleeNerf.AddXP);
-
-
+--print("Lanttuchef's Melee weapon initialize events")
 Events.OnGameStart.Add(LanttuMeleeNerf.InitializeEvents)
